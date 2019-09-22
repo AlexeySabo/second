8a71b8a80bd569725861dcef9fc33aac1b8b4d37
AlexeySabo <alexeysabo@icloud.com>
2019-09-22 20:43:23 +0300

    commit_one

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="rus">
+<head>
+    <title>Тест формы</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h3>Форма ввода</h3>
+<form action="index.php" method="post" enctype="multipart/form-data">
+    Логин<br>
+    <input type="text" name="login"><br>
+    Логин<br>
+    <input type="password" name="pass"><br>
+    Отправить<br>
+    <input type="submit" value="Отправить"><br>
+</form>
+</body>
+</html>
+<?php
+$post = $_POST;
+echo $post['login'].' '.$post['pass'];
+$i=$i+1;
+$array[] = " {$post['login']} {$post['pass']}";
+echo $i;
+?>
